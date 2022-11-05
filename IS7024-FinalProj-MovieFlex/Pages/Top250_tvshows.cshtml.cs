cb0d49f59234a71fa175caade61e316564b69ae6
aruntosky <dubeyarunkumar24@gmail.com>
2022-11-05 18:06:25 -0400

    Branch testing with new razor page

---
+++ IS7024-FinalProj-MovieFlex/Pages/Top250_tvshows.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7024_FinalProj_MovieFlex.Pages
+{
+    public class Top250_tvshowsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
