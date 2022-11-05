568dd83f3e0069e2da7c1ebf1790c45b020653de
SauravNandi0908 <saurav0908.nandi@gmail.com>
2022-11-05 18:04:51 -0400

    Made a contact_us page

---
+++ IS7024-FinalProj-MovieFlex/Contact Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7024_FinalProj_MovieFlex
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
