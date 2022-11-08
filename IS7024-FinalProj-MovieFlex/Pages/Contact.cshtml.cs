e8de259f7ddb9304f4f998bbe9516d990170493c
JaswanthBathi <112015478+JaswanthBathi@users.noreply.github.com>
2022-11-08 16:36:18 -0500

    developed a new contact us page from scratch

---
+++ IS7024-FinalProj-MovieFlex/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7024_FinalProj_MovieFlex.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
