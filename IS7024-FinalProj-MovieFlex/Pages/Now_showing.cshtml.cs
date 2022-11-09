4866e1206a93c33faa940f57f05221f4481bd75b
SauravNandi0908 <saurav0908.nandi@gmail.com>
2022-11-08 21:04:18 -0500

    JSon integration of BoxOffice Api done here in Now_showing.cshtml.cs

---
+++ IS7024-FinalProj-MovieFlex/Pages/Now_showing.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using MovieShows;
+
+namespace IS7024_FinalProj_MovieFlex.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public void OnGet()
+        {
+
+            var task = client.GetAsync("https://imdb-api.com/en/API/BoxOffice/k_thj97up3");
+            HttpResponseMessage result = task.Result;
+            ShowingMovies Show_movie_list = new ShowingMovies();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                Show_movie_list = ShowingMovies.FromJson(jsonString);
+            }
+            ViewData["Show_movie_list"] = Show_movie_list;
+
+        }
+    }
+}
