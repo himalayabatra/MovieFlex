9841e4ccdf1beaa91e4912ca12fa5db0df727a21
Krunal Shigavan <krunal.shigavan@gmail.com>
2022-11-08 19:13:39 -0500

    Created Top 250 Movies Razor Page Created MovieList class to read JSON data Updated Index Layout page for re-direction to Top 250 Movies Page

--- IS7024-FinalProj-MovieFlex/Pages/Top250Movies.cshtml.cs
+++ IS7024-FinalProj-MovieFlex/Pages/Top250Movies.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Movie;
 
 namespace IS7024_FinalProj_MovieFlex.Pages
 {
     public class Top250MoviesModel : PageModel
     {
+        static readonly HttpClient client = new HttpClient();
         public void OnGet()
         {
+           var task = client.GetAsync("https://imdb-api.com/en/API/Top250Movies/k_thj97up3");
+            HttpResponseMessage result =  task.Result;
+            ////List<MovieList> movies = new List<MovieList>();
+            MovieList movies = new MovieList();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                movies = MovieList.FromJson(jsonString);
+            }
+            ViewData["Movies"] = movies;
         }
     }
 }
