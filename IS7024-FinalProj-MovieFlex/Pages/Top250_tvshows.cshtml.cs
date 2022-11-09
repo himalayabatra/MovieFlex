50a65455b8250647b89599156e716a4698959c2c
aruntosky <dubeyarunkumar24@gmail.com>
2022-11-08 21:12:28 -0500

    IMDB Top 250 Shows API Integration -Top250_tvshows.cshtml.cs

--- IS7024-FinalProj-MovieFlex/Pages/Top250_tvshows.cshtml.cs
+++ IS7024-FinalProj-MovieFlex/Pages/Top250_tvshows.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shows;
 
 namespace IS7024_FinalProj_MovieFlex.Pages
 {
     public class Top250_tvshowsModel : PageModel
     {
+        static readonly HttpClient client= new HttpClient();
         public void OnGet()
         {
+            var task = client.GetAsync("https://imdb-api.com/en/API/Top250TVs/k_thj97up3");
+            HttpResponseMessage result = task.Result;
+            Tvshows tvshows = new Tvshows();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readstring = result.Content.ReadAsStringAsync();
+                string jsonString = readstring.Result;
+                tvshows = Tvshows.FromJson(jsonString);
+            }
+            ViewData["Tvshows"] = tvshows;
         }
     }
 }
